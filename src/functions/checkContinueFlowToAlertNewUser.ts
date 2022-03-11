aa64d3b4879eaf5ca9cf9f14cc4b16d0a9b7a06c
angeloevangelista <angeloevan.ane@gmail.com>
2022-03-10 21:19:30 -0300

    not alerting when bot enters the room

--- src/functions/checkContinueFlowToAlertNewUser.ts
+++ src/functions/checkContinueFlowToAlertNewUser.ts
@@ -5,13 +5,14 @@ async function checkContinueFlowToAlertNewUser(
   previousState: VoiceState,
   nextState: VoiceState
 ): Promise<boolean> {
+  const isNotBot = !nextState.member?.user.bot;
   const isNotBotEntrance = nextState.member?.user.id !== client.user?.id;
   const isNewUserOnChat = previousState.channelID !== nextState.channelID;
 
   const isMoreThanOneUser =
     (nextState.channel?.members.map((p) => p.id).length || 0) > 1;
 
-  return [isNotBotEntrance, isNewUserOnChat, isMoreThanOneUser].every(
+  return [isNotBot, isNotBotEntrance, isNewUserOnChat, isMoreThanOneUser].every(
     (condition) => condition
   );
 }
